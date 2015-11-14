c6649ccf438db408d40da0dde2d83434e11951bf
Ross Mackay <ross@mcky.me>
2015-11-14 19:29:28 +0000

    Add actions api endpoint

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,21 @@
 var express = require('express')
 	, router = express.Router()
 	, recipeController = require('../controllers/recipeController')
+	, actions = require('../controllers/actionController')
 
 var toJSON = function(req, res) {
 	console.log('??')
   var data = res.locals.data
   res.json(data)
 }
+
+router.route('/actions')
+	.get(function(req, res, next) {
+		var actionList = Object.keys(actions)
+		res.json({
+			actions: actionList
+		})
+	})
 
 router.route('/:userId/recipes')
 	.get(recipeController.list)
