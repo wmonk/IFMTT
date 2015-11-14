ae7a7afaed0b1e0b97514429df955e4e5ffa24f8
Will Monk <william.monk@skybettingandgaming.com>
2015-11-14 12:11:52 +0000

    Added index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hey!');
+});
+
+app.listen(3000, function (err) {
+    if (err) {
+        throw err;
+    }
+
+    console.log('Listening on port 3000; http://localhost:3000');
+});
