26c67f2ac01bba73ea883e0557a7a3f314fb430e
Krishan Patel <krishan711@gmail.com>
2015-11-15 12:17:04 +0000

    added day and weekday triggers

--- controllers/triggerController.js
+++ controllers/triggerController.js
@@ -25,5 +25,15 @@ module.exports = {
     station: function (transaction, arguments) {
         return transaction.merchant.name == 'Transport For London' && transaction.merchant.address.city.toLowerCase() == arguments.station.toLowerCase();
     },
+    
+    day: function (transaction, arguments) {
+        var date = moment(transaction.created).isoWeeday(1);
+        return date.day() == arguments.day % 7;
+    },
+    
+    weekday: function (transaction, arguments) {
+        var date = moment(transaction.created).isoWeeday(1);
+        return date.day() < 5;
+    },
 
 }
