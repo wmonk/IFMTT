7320a8b2548b851678570f79ac861993941d52a9
Ollie <olliemonk0@gmail.com>
2015-11-15 14:25:43 +0000

    login update

--- client/src/scripts/components/loginForm.js
+++ client/src/scripts/components/loginForm.js
@@ -26,8 +26,18 @@ let LoginForm = React.createClass({
 		return (<div className="container">
 			<img className="login-logo m-b" src={'img/mondo.svg'} />
 			<form onSubmit={this.formSubmit}>
-				<input placeholder="username" className="m-b p-all w-12" name="username" ref={(c) => this._username = c}/>
-				<input placeholder="password" className="m-b p-all w-12" name="password" type="password" ref={c => this._password = c}/>
+				<input 
+					placeholder="username" 
+					className="m-b p-all w-12"
+					name="username" 
+					type="email" 
+					ref={(c) => this._username = c}/>
+				<input 
+					placeholder="password" 
+					className="m-b p-all w-12" 
+					name="password" 
+					type="password" 
+					ref={c => this._password = c}/>
 				<button className="btn-primary m-b p-all w-12" type="submit">submit</button>
 			</form>
 		</div>);
