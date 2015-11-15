483c30936c651876225a63314b2c67e2d140dcb5
Ollie <olliemonk0@gmail.com>
2015-11-15 12:13:07 +0000

    init login form

--- client/src/scripts/main.js
+++ client/src/scripts/main.js
@@ -1,8 +1,39 @@
 import React from 'react';
 import { render } from 'react-dom';
+import request from 'superagent'
 
-render(
-<div className="container">
-	<img src="{}" />
-</div>, document.getElementById('root')
+let App = React.createClass({
+	displayName: 'App',
+
+	formSubmit(e) {
+		e.preventDefault();				
+
+		let username = this._username.value;
+		let password = this._password.value;
+
+		request
+			.post('/login')
+			.send({
+				username,
+				password
+			})
+			.end((err, body) => {
+				console.log(body)
+			});
+	},
+
+	render() {
+		return (<div>
+			<form onSubmit={this.formSubmit}>
+				<input name="username" ref={(c) => this._username = c}/>
+				<input name="password" type="password" ref={c => this._password = c}/>
+				<button type="submit">submit</button>
+			</form>			
+		</div>);
+	}
+})
+
+render(<App />, document.getElementById('root')
 );
+
+
